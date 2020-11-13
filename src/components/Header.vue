ac30f09f8a7cdfcd20deec10adcbdd3eb08c4723
GurYou <63901149+fcaroliAdmin@users.noreply.github.com>
2020-11-13 23:05:03 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,15 +4,15 @@
             <div class="left">
                 <g-link :to="{ name: 'home' }" class="home-link">
                     <img 
-                        src="../../static/logo.svg"
+                        src="../../static/ETLogo.png"
                         :alt="settings.site_name" 
                         class="logo"
                     />
                 </g-link>
             </div>
             <nav class="nav right">
-                <g-link class="nav__link" to="/journal">Journal</g-link>
-                <g-link class="nav__link" to="/contact">Say Hi!</g-link>
+                <g-link class="nav__link" to="/journal">Case Studies</g-link>
+                <g-link class="nav__link" to="/contact">Lets talk!</g-link>
             </nav>
         </div>
     </header>
@@ -22,7 +22,7 @@
 export default {
   data() {
     return {
-        logo: require("../../static/logo.svg"),
+        logo: require("../../static/ETLogo.png"),
         settings: require("../../data/theme.json")
     }
   }

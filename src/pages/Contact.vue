05d847cf6d3ec2e399e18cf3232b45fd6ba5a46a
GurYou <63901149+fcaroliAdmin@users.noreply.github.com>
2020-11-14 06:34:02 +0100

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -3,8 +3,8 @@
     <div class="container">
 
       <div class="contact-header">
-        <h1 class="contact-title">Say hi!</h1>
-        <p>Leave me a note with any questions you might have, I'll get back to you as soon as possible.</p>
+        <h1 class="contact-title">Need a solution on AWS, EquioTech would like to speak with you!</h1>
+        <p>Leave me a note with any questions you might have, We will get back to you as soon as possible.</p>
       </div>
 
       <form class="contact-form" name="contact">

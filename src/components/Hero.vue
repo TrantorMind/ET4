a572745bea0b4165d1ce2da78cc6476381cce06a
Francesco Caroli <francesco@equiotech.com>
2020-11-14 19:53:08 +0100

    Small changes
    Nice

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,6 @@
         <h1 class="hero-title" v-html="settings.hero_title" />
         <h2 class="hero-subtitle" v-html="settings.hero_subtitle" />
     </div>
-    <button class="button"> Lets Talk</button>
 </template>
 
 <script>

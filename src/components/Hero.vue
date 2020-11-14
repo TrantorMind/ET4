44f32a8cfe3f0a6bede43c3f7e1fee37a1d4bfe3
GurYou <63901149+fcaroliAdmin@users.noreply.github.com>
2020-11-14 08:39:35 +0100

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,6 +3,27 @@
         <h1 class="hero-title" v-html="settings.hero_title" />
         <h2 class="hero-subtitle" v-html="settings.hero_subtitle" />
     </div>
+    <form class="contact-form" name="contact">
+
+        <div class="sender-info">
+          <div>
+            <label for="name" class="label">Your name</label>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label for="email" class="label">Your email</label>
+            <input type="email" name="email" />
+          </div>
+        </div>
+
+        <div class="message">
+          <label for="message" class="label">Message</label>
+          <textarea name="message"></textarea>
+        </div>
+
+        <button class="button">Submit form</button>
+
+   </form>
 </template>
 
 <script>
